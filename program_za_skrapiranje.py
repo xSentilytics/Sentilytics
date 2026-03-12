6319ecb3234d5ee9695a59cb44cc9034931fc9b4
ngamulin02 <nikolagamulin@icloud.com>
2026-03-12 16:37:30 +0100

    Update program_za_skrapiranje.py

--- program_za_skrapiranje.py
+++ program_za_skrapiranje.py
@@ -2,46 +2,72 @@ from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
-from selenium.common.exceptions import TimeoutException
+from selenium.common.exceptions import TimeoutException, NoSuchElementException
+import time
 import nltk
 import csv
+
 from doktori import DOKTORI
+
 
 def skrapiranje_komentara(naziv_csv):
     with open(naziv_csv + ".csv", "w", newline="", encoding="utf-8") as f:
-        w = csv.writer(f, delimiter='\t')
+        w = csv.writer(f, delimiter="\t")
         w.writerow(["groupid", "url", "title", "review_id", "sentence_id", "text"])
-        review_id = 0
+
         tokenizer = nltk.data.load("tokenizers/punkt/slovene.pickle")
-        preglednik = webdriver.Safari()
+        review_id = 0
+
         for doktor in DOKTORI:
-# promijeni preglednik ako ne koristiš Safari!
+
+            # promijeni preglednik ako ne koristiš Safari!
+            preglednik = webdriver.Safari()
             preglednik.get(DOKTORI[doktor])
-            print("Otvoren profil doktora: ", doktor)
-#zatvaranje prozora za kolačiće
+
+            cekanje = WebDriverWait(preglednik, 10)
+
+            # zatvaranje prozora za kolačiće
             try:
-                gumb_pristanak = WebDriverWait(preglednik, 5).until(EC.element_to_be_clickable((By.CSS_SELECTOR, "button[aria-label='Consent']")))
+                gumb_pristanak = cekanje.until(
+                    EC.element_to_be_clickable(
+                        (By.CSS_SELECTOR, "button[aria-label='Consent']")
+                    )
+                )
                 preglednik.execute_script("arguments[0].click();", gumb_pristanak)
                 print("Prozor za kolačiće zatvoren!")
+
             except TimeoutException:
                 print("Nije pronađen prozor za kolačiće!")
-#gumb "učitaj još komentara"
+
+            # gumb "učitaj još komentara"
             while True:
                 try:
-                    gumb_ucitaj_vise = WebDriverWait(preglednik, 5).until(EC.element_to_be_clickable((By.ID, "load-more-comments")))
+                    gumb_ucitaj_vise = preglednik.find_element(By.ID, "load-more-comments")
+
+                    if not gumb_ucitaj_vise.is_displayed():
+                        break
+
                     preglednik.execute_script("arguments[0].click();", gumb_ucitaj_vise)
                     print("Učitavanje komentara...")
-                except TimeoutException:
-                    break
-#skrapiranje komentara
-            for komentar in preglednik.find_elements(By.CSS_SELECTOR, "div.comment p"):
-                review_id+=1
-                recenice = tokenizer.tokenize(komentar.text.strip())
-                for sentence_id, recenica in enumerate(recenice, start=1):
-                    w.writerow([1, DOKTORI[doktor], doktor, review_id, sentence_id, recenica])
+                    time.sleep(1)
 
-        preglednik.quit()
+                except NoSuchElementException:
+                    break
+
+            # skrapiranje komentara
+            for komentar in preglednik.find_elements(By.CSS_SELECTOR, "div.comment p"):
+                review_id += 1
+                recenice = tokenizer.tokenize(komentar.text.strip())
+
+                for sentence_id, recenica in enumerate(recenice, start=1):
+                    w.writerow([1,DOKTORI[doktor],doktor,review_id,sentence_id,recenica])
+
+            preglednik.quit()
+
 
 if __name__ == "__main__":
-    naziv_csv = input("Unesi naziv CSV datoteke u koju želiš spremiti rečenice (Bez .csv!):")
+    naziv_csv = input("Unesi naziv CSV datoteke u koju želiš spremiti rečenice (Bez .csv!): ")
     skrapiranje_komentara(naziv_csv)
+
+
+
