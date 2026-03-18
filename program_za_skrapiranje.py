aa50422e1025a5c9375a4ad3b37067e6a9482064
ngamulin02 <nikolagamulin@icloud.com>
2026-03-18 15:40:16 +0100

    Change comment scraping output to Excel format
    Refactor scraping function to save comments in Excel format instead of CSV.

--- program_za_skrapiranje.py
+++ program_za_skrapiranje.py
@@ -5,65 +5,68 @@ from selenium.webdriver.support import expected_conditions as EC
 from selenium.common.exceptions import TimeoutException, NoSuchElementException
 import time
 import nltk
-import csv
+
+from openpyxl import Workbook
 
 from doktori import DOKTORI
 
+def skrapiranje_komentara(naziv_xlsx):
+    wb = Workbook()
+    ws = wb.active
+    ws.title = "Komentari"
 
-def skrapiranje_komentara(naziv_csv):
-    with open(naziv_csv + ".csv", "w", newline="", encoding="utf-8") as f:
-        w = csv.writer(f, delimiter="\t")
-        w.writerow(["groupid", "url", "title", "review_id", "sentence_id", "text"])
-        tokenizer = nltk.data.load("tokenizers/punkt/slovene.pickle")
-        review_id = 0
-        preglednik = webdriver.Safari()
+    ws.append(["groupid", "url", "title", "review_id", "sentence_id", "text"])
 
-        for doktor in DOKTORI:
+    tokenizer = nltk.data.load("tokenizers/punkt/slovene.pickle")
+    review_id = 0
+    preglednik = webdriver.Safari()
 
-            # promijeni preglednik ako ne koristiš Safari!
-            preglednik.get(DOKTORI[doktor])
-            print("Otvoren URL doktora:", doktor)
+    for doktor in DOKTORI:
 
-            cekanje = WebDriverWait(preglednik, 2)
+        preglednik.get(DOKTORI[doktor])
+        print("Otvoren URL doktora:", doktor)
 
-            # zatvaranje prozora za kolačiće
-            try:
-                gumb_pristanak = cekanje.until(
-                    EC.element_to_be_clickable(
-                        (By.CSS_SELECTOR, "button[aria-label='Consent']")
-                    )
+        cekanje = WebDriverWait(preglednik, 2)
+
+        try:
+            gumb_pristanak = cekanje.until(
+                EC.element_to_be_clickable(
+                    (By.CSS_SELECTOR, "button[aria-label='Consent']")
                 )
-                preglednik.execute_script("arguments[0].click();", gumb_pristanak)
-                print("Prozor za kolačiće zatvoren!")
+            )
+            preglednik.execute_script("arguments[0].click();", gumb_pristanak)
+            print("Prozor za kolačiće zatvoren!")
 
-            except TimeoutException:
-                print("Nije pronađen prozor za kolačiće!")
+        except TimeoutException:
+            print("Nije pronađen prozor za kolačiće!")
 
-            # gumb "učitaj još komentara"
-            while True:
-                try:
-                    gumb_ucitaj_vise = preglednik.find_element(By.ID, "load-more-comments")
+        # Load more comments
+        while True:
+            try:
+                gumb_ucitaj_vise = preglednik.find_element(By.ID, "load-more-comments")
 
-                    if not gumb_ucitaj_vise.is_displayed():
-                        break
-
-                    preglednik.execute_script("arguments[0].click();", gumb_ucitaj_vise)
-                    print("Učitavanje komentara...")
-                    time.sleep(1)
-
-                except NoSuchElementException:
+                if not gumb_ucitaj_vise.is_displayed():
                     break
 
-            # skrapiranje komentara
-            for komentar in preglednik.find_elements(By.CSS_SELECTOR, "div.comment p"):
-                review_id += 1
-                recenice = tokenizer.tokenize(komentar.text.strip())
+                preglednik.execute_script("arguments[0].click();", gumb_ucitaj_vise)
+                print("Učitavanje komentara...")
+                time.sleep(1)
 
-                for sentence_id, recenica in enumerate(recenice, start=1):
-                    w.writerow([1,DOKTORI[doktor],doktor,review_id,sentence_id,recenica])
+            except NoSuchElementException:
+                break
 
-        preglednik.quit()
+        for komentar in preglednik.find_elements(By.CSS_SELECTOR, "div.comment p"):
+            review_id += 1
+            recenice = tokenizer.tokenize(komentar.text.strip())
+
+            for sentence_id, recenica in enumerate(recenice, start=1):
+                ws.append([1,DOKTORI[doktor],doktor,review_id,sentence_id,recenica])
+
+    preglednik.quit()
+
+    # Save Excel file
+    wb.save(naziv_xlsx + ".xlsx")
 
 if __name__ == "__main__":
-    naziv_csv = input("Unesi naziv CSV datoteke u koju želiš spremiti rečenice (Bez .csv!): ")
-    skrapiranje_komentara(naziv_csv)
+    naziv = input("Unesi naziv Excel datoteke (bez .xlsx): ")
+    skrapiranje_komentara(naziv)
